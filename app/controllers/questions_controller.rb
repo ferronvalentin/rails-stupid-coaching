164b625d49fb6246a301b7da2bab29b8c8512862
Valentin Ferron <ferron.vale@gmail.com>
2017-11-06 13:56:00 +0100

    fisrt exercice

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,8 +2,15 @@ class QuestionsController < ApplicationController
 
 
   def answer
+    @ask = params[:query]
+    if @ask == "yes"
+      @answer = "la réponse du coach"
+    else
+      @answer = "différent"
+    end
   end
 
   def ask
+
   end
 end
